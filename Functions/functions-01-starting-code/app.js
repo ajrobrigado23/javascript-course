7c11a1b7982ff47fffab590031bc6b4c0b94eaaf
Albert Robrigado <albertrobrigado.ict@gmail.com>
2025-10-03 19:26:38 +0800

    add and learn javascript functions

--- Functions/functions-01-starting-code/app.js
+++ Functions/functions-01-starting-code/app.js
@@ -4,4 +4,15 @@ function startGame() {
     console.log("Game is starting...");
 }
 
-startGameBtn.addEventListener("click", startGame);
+// Anonymous function
+const start = function() {
+  console.log("Game is starting inside of this anonymous function...");
+};
+
+// Function declaration and function expressions
+start();
+
+// (Event listener function ) - Passing a reference to this anonymous function
+startGameBtn.addEventListener("click", function() {
+    console.log("Anonymous function that is inside of a event listener function currently running...");
+});
