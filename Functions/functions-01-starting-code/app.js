27260f2cfa8ddfcb377205346f6a88fa1f2d7d3a
Albert Robrigado <albertrobrigado.ict@gmail.com>
2025-10-04 11:45:30 +0800

    add rock, paper and scissors game.

--- Functions/functions-01-starting-code/app.js
+++ Functions/functions-01-starting-code/app.js
@@ -12,7 +12,42 @@ const start = function() {
 // Function declaration and function expressions
 start();
 
+// Rock Paper and Scissors
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_SELECT = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerSelection = function() {
+
+  const playerSelect = prompt(`Pick: ${ROCK}, ${PAPER} or ${SCISSORS}`).toUpperCase();
+
+  if (playerSelect !== ROCK &&
+      playerSelect !== PAPER &&
+      playerSelect !== SCISSORS) {
+
+      alert(`Invalid choice! We chose ${DEFAULT_USER_SELECT} for you!`);
+
+      return DEFAULT_USER_SELECT;
+
+  }
+
+  return playerSelect;
+
+};
+
 // (Event listener function ) - Passing a reference to this anonymous function
 startGameBtn.addEventListener("click", function() {
+
+    if (gameIsRunning) {
+        return;
+    }
+
+    gameIsRunning = true;
+
     console.log("Anonymous function that is inside of a event listener function currently running...");
+    const playerSelection = getPlayerSelection();
+    console.log(playerSelection);
 });
