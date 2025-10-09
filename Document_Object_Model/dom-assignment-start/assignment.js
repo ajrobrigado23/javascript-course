cd3c5f9a290946e47d3e28cfe63cba2f329b6db2
Albert Robrigado <albertrobrigado.ict@gmail.com>
2025-10-09 17:05:21 +0800

    finished the dom assignment

---
+++ Document_Object_Model/dom-assignment-start/assignment.js
@@ -0,0 +1,12 @@
+const taskOne = document.getElementById("task-1");
+// Change the background color to black and text-white
+taskOne.style.backgroundColor = "black";
+taskOne.style.color = "white";
+
+// Change the document title
+const taskTwo = document.querySelector("head title");
+taskTwo.textContent =  "Assignment - Solved!";
+
+// Select the h1 title
+const taskThree = document.querySelector("h1");
+taskThree.textContent = "Assignment - Solved!";
