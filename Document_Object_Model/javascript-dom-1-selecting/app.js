415bfcf98cb4f3519396a26011eb021065b663d0
Albert Robrigado <albertrobrigado.ict@gmail.com>
2025-10-09 09:50:21 +0800

    add and learning document object model

--- Document_Object_Model/javascript-dom-1-selecting/app.js
+++ Document_Object_Model/javascript-dom-1-selecting/app.js
@@ -1,3 +1,14 @@
 const h1 = document.querySelector("h1");
 
 console.dir(h1);
+
+// Modern way to get multiple elements
+const listItemElements = document.getElementsByTagName("li");
+
+for(let itemElement of listItemElements) {
+    console.dir(itemElement);
+}
+
+// access to the last element
+const lastItemElement = document.querySelector("li:last-of-type");
+lastItemElement.textContent = "(Change!)";
