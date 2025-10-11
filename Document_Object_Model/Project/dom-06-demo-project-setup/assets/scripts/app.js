38c4d2c11eeb7154707be8ef19149617f6a5b3ba
Albert Robrigado <albertrobrigado.ict@gmail.com>
2025-10-11 10:25:37 +0800

    select all the buttons

---
+++ Document_Object_Model/Project/dom-06-demo-project-setup/assets/scripts/app.js
@@ -0,0 +1,7 @@
+// First goal is to show the modal
+const addMovieModal = document.getElementById('add-modal');
+// const addMovieModal = document.body.children[1];
+
+const startAddMovieButton = document.querySelector('header button');
+// const startAddMovieButton = document.querySelector('header').lastElementChild;
+
