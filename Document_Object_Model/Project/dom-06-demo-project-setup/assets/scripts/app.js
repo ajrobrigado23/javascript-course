81dbb465ad076fe8a99803d445a47d209944339a
Albert Robrigado <albertrobrigado.ict@gmail.com>
2025-10-11 16:32:11 +0800

    remove the backdrop when it clicks and add cancel button.

--- Document_Object_Model/Project/dom-06-demo-project-setup/assets/scripts/app.js
+++ Document_Object_Model/Project/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,22 +1,38 @@
 // First goal is to show the modal
-const addMovieModal = document.getElementById('add-modal');
+const addMovieModalElement = document.getElementById('add-modal');
 // const addMovieModal = document.body.children[1];
 
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
 
-// Next one is opening the modal
-startAddMovieButton.addEventListener('click', () => {
-    // Check the class if it's visible or not (the other class will remain it will just add the new class)
-    addMovieModal.classList.toggle('visible');
-    // Call the function (backdrop)
-    toggleBackdrop();
-});
+// Step-2 Toggle the id backdrop
+const backdropElement = document.getElementById('backdrop');
 
-// Toggle the id backdrop
-const backdrop = document.getElementById('backdrop');
+// Select the cancel button
+const cancelAddMovieButton = addMovieModalElement.querySelector('.btn--passive');
 
 // Create a function that will toggle the backdrop
 function toggleBackdrop() {
-    backdrop.classList.toggle('visible');
+    backdropElement.classList.toggle('visible');
 }
+
+function toggleMovieModal() {
+    // Check the class if it's visible or not (the other class will remain it will just add the new class)
+    addMovieModalElement.classList.toggle('visible');
+    // Call the function (backdrop)
+    toggleBackdrop();
+}
+
+function backdropClickHandler() {
+    // Close the modal and Close the backdrop
+    toggleMovieModal();
+}
+
+// Next one is opening the modal
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+
+// Close the modal when cancel is clicked
+cancelAddMovieButton.addEventListener('click', backdropClickHandler);
+
+//  Close the modal when backdrop is clicked
+backdropElement.addEventListener('click', backdropClickHandler);
