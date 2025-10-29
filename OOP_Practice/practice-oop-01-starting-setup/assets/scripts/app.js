ed21d643cafd94ec48e810de41c70e7e1e80be19
Albert Robrigado <albertrobrigado.ict@gmail.com>
2025-10-29 18:36:01 +0800

    learn how to use the classes

--- OOP_Practice/practice-oop-01-starting-setup/assets/scripts/app.js
+++ OOP_Practice/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -4,11 +4,14 @@ class Tooltip {
 
 class ProjectItem {
 
-    constructor(id) {
+    constructor(id, updateProjectLists) {
         // Stored the id (as an instance property)
         this.id = id;
+        this.updateProjectListsHandler = updateProjectLists;
         this.connectedElement = document.getElementById(id);
         this.connectDragTarget = this.connectedElement.querySelector('h2');
+
+        this.connectSwitchButton();
     }
 
     connectMoreInfoButton() {
@@ -21,9 +24,7 @@ class ProjectItem {
         // Select the last button
         const switchButton = projectItemElement.querySelector('button:last-of-type');
         // Switch the list and we gonna use the callback function for that.
-        switchButton.addEventListener('click', () => {
-
-        });
+        switchButton.addEventListener('click', this.updateProjectListsHandler);
 
     }
 
@@ -34,29 +35,45 @@ class ProjectList {
     projects = [];
 
     constructor(type) {
+        this.type = type;
         // Get all the related items
         const projectItems = document.querySelectorAll(`#${type}-projects li`);
         // Loop through them and store in our array (instances of project items)
         for (const projectItem of projectItems) {
             // Stored the project item in our array using the project item id
-            this.projects.push(new ProjectItem(projectItem.id));
+            this.projects.push(
+                new ProjectItem(projectItem.id, this.switchProject.bind(this))
+            );
         }
         console.log(this.projects);
     }
 
-    addProject(project) {
+    setSwitchHandlerFunction(switchHandlerFunction) {
+        this.switchHandler = switchHandlerFunction;
+    }
+
+    addProject() {
         // Add the project to the array
-        this.projects.push(project);
+        // this.projects.push(project);
         // Switch the element away (the project)
+        console.log(this);
     }
 
     // Switch the element away (the project)
     switchProject(projectId) {
-        // Remove to the project array
-        const project = this.projects.findIndex(project => project.id === projectId);
-        this.projects.splice(project, 1);
+        // 1 Option
+        // Remove to the project array (find the index first)
+        // const project = this.projects.findIndex(project => project.id === projectId);
+        // Once you find the index you want to delete use splice to remove it
+        // this.projects.splice(project, 1);
+
+        // Switch the element away (the project)
+        this.switchHandler(this.projects.find(p => p.id === projectId));
+
+        // 2 Option
         // Alternative (removing an item)
         this.projects = this.projects.filter(project => project.id !== projectId);
+
     }
 }
 
@@ -65,6 +82,15 @@ class App {
         // Create the two project type
         const activeProjectList = new ProjectList('active');
         const finishedProjectList = new ProjectList('finished');
+
+        // Set the switch handler function
+        activeProjectList.setSwitchHandlerFunction(
+            finishedProjectList.addProject.bind(finishedProjectList)
+        );
+
+        finishedProjectList.setSwitchHandlerFunction(
+            activeProjectList.addProject.bind(activeProjectList)
+        );
     }
 }
 
